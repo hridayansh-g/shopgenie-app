9e9a54947a7988bf0cdc980312a462d04d51b5c5
Hridayansh Gupta <hridayansh@Hridayanshs-MacBook-Air.local>
2025-07-11 21:20:35 +0530

    🔧 Updated API_BASE_URL to Render live backend

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "http://192.168.1.13:5000";
+//export const API_BASE_URL = "http://192.168.1.13:5000";
+export const API_BASE_URL = "https://shopgenie-backend-lm88.onrender.com";
