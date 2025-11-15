5a003865ea40b598a3b4b12235f9d9343c0d3bf6
Hridayansh Gupta <hridayansh@Hridayanshs-MacBook-Air.local>
2025-11-15 14:09:11 +0530

    Updated ShopGenie frontend: Navigation fixes, layout fixes, removed reanimated import, prepared for Expo publish

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,8 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      require('expo-router/babel'),
-    ],
+    plugins: ['react-native-reanimated/plugin'], // last plugin
   };
 };
